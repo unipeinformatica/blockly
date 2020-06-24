1a3d160eb74afa7f793c83c24d194fbf5be50f60
Gustavo Del Dago <gdeldago@gmail.com>
2020-06-24 10:46:22 -0300

    Agrego funciones de consola Python.

---
+++ python/copiar-la-entrada.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#from consola import *
+from consola import leer_caracter
+from consola import leer_entrada_completa
+from consola import obtener_caracter
+from consola import avanzar_caracter
+from consola import hay_mas_caracteres
+from consola import imprimir
+from consola import cambiar_color_texto
+
+# =============================================================================
+#
+# while hay_mas_caracteres():
+#     imprimir(leer_caracter())
+#     avanzar_caracter()
+#
+#
+# cambiar_color_texto('#ff4040')
+# imprimir('Hola\n')
+# cambiar_color_texto('#009900')
+# imprimir('Manola\n')
+# cambiar_color_texto('#FF44AA')
+# imprimir('ATC Argentina Telefivosa Color')
+#
+# =============================================================================
+for n in range(11):
+    imprimir(obtener_caracter())
+
---
+++ python/pepe.py
@@ -0,0 +1,9 @@
+from consola.py import leer_caracter
+from consola.py import leer_entrada_completa
+from consola.py import obtener_caracter
+from consola.py import avanzar_caracter
+from consola.py import hay_mas_caracteres
+from consola.py import imprimir
+from consola.py import cambiar_color_texto
+
+imprimir('Hola')
